9f527e684c36ace013ac8d5fdff75691d5a65f6f
Rehan Habib <rehan.habib777@gmail.com>
2023-11-24 16:51:34 +0500

    Change Log format in update_records management command

--- customer/management/commands/update_records.py
+++ customer/management/commands/update_records.py
@@ -25,7 +25,7 @@ class Command(BaseCommand):
                 record.is_utc = False
                 record.is_pst = True
                 record.save()
-                logger.info(f"Customer {record.id} time updated in pst format {record.Time}")
+                logger.info(f"Customer {record.id} time in utc format {utc_time} is updated in pst format {record.Time}")
             self.update_count(count)
 
         elif (Customer.objects.filter(is_pst = False).count() == count and count<100):
@@ -37,7 +37,7 @@ class Command(BaseCommand):
                 record.is_utc = True
                 record.is_pst=False
                 record.save()
-                logger.info(f"Customer {record.id} time updated in utc format {record.Time}")
+                logger.info(f"Customer {record.id} time in pst_format {pst_time} is updated in utc format {record.Time}")
             self.update_count(count)
         elif count ==100:
             count=0
@@ -54,9 +54,6 @@ class Command(BaseCommand):
         iteration_file_path = os.path.join(BASE_DIR, "CountIteration.txt")
 
         if count < 100 :
-            count += 10     
-        else:
-            count = 0
-
+            count += 10
         with open(iteration_file_path, "w") as f:
             f.write(str(count))
