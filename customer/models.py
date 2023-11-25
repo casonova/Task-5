722601986f024d0069eeffe41b2456b54ad05e51
Rehan Habib <rehan.habib777@gmail.com>
2023-11-25 19:37:44 +0500

    Changed models.py

--- customer/models.py
+++ customer/models.py
@@ -1,11 +1,11 @@
-from django.db import models
 from datetime import datetime
+
+from django.db import models
 from django.utils import timezone
 
 
 class Customer(models.Model):
-    name= models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     Time = models.DateTimeField()
     is_utc = models.BooleanField()
-    is_pst=models.BooleanField()
-  
+    is_pst = models.BooleanField()
